a1383d429df9468f9acfa3ba713c2191b06dc12d
wanggang <wanggy01@servyou.com.cn>
2022-10-20 21:24:24 +0800

    feat-flink增量窗口聚合函数

---
+++ src/main/java/me/demo/flink/watermark/WindowAggregatorTestPvUv.kt
@@ -0,0 +1,67 @@
+package me.demo.flink.watermark
+
+import me.demo.flink.source.CustomSource
+import me.demo.flink.source.Event
+import org.apache.flink.api.common.eventtime.WatermarkStrategy
+import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.api.java.tuple.Tuple2
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.time.Time
+
+class WindowAggregatorTestPvUv {
+
+
+}
+
+fun main() {
+    // 创建执行环境
+    val env = StreamExecutionEnvironment.getExecutionEnvironment()
+    env.parallelism = 1
+
+    // 计窗时间窗口内，用户的PV/UV的比值：表示站点的用户粘性
+    val stream: SingleOutputStreamOperator<Event> = env.addSource(CustomSource())
+        .assignTimestampsAndWatermarks(
+            WatermarkStrategy.forMonotonousTimestamps<Event>()
+                .withTimestampAssigner { element, _ -> element.timestamp }
+        )
+    stream.print("data")
+
+    val avgOutput: SingleOutputStreamOperator<String> = stream.keyBy { true }
+        .window(SlidingEventTimeWindows.of(Time.seconds(10), Time.seconds(2)))
+        .aggregate(AvgPVFun())
+    avgOutput.print("APT:TumblingEventTimeWindows")
+    env.execute()
+}
+
+
+/***
+ * IN：Event
+ * ACC：Tuple2<访问时间,次数>
+ * OUT：平均访问时间
+ */
+class AvgPVFun : AggregateFunction<Event, Tuple2<Long, HashSet<String>>, String> {
+    override fun createAccumulator(): Tuple2<Long, HashSet<String>> {
+        return Tuple2.of(0, HashSet())
+    }
+
+    override fun add(value: Event, accumulator: Tuple2<Long, HashSet<String>>): Tuple2<Long, HashSet<String>> {
+        // 每条数据，pv+1，将user放入hashset
+        accumulator.f1.add(value.user)
+        return Tuple2.of(accumulator.f0 + 1, accumulator.f1)
+    }
+
+    override fun getResult(accumulator: Tuple2<Long, HashSet<String>>): String {
+        //窗口触发时，输出pv和uv的比值
+        return (accumulator.f0 / accumulator.f1.size.toDouble()).toString()
+    }
+
+    override fun merge(
+        a: Tuple2<Long, HashSet<String>>,
+        b: Tuple2<Long, HashSet<String>>,
+    ): Tuple2<Long, HashSet<String>> {
+        a.f1.addAll(b.f1)
+        return Tuple2.of(a.f0 + b.f0, a.f1)
+    }
+}
---
+++ src/main/java/me/demo/flink/watermark/WindowTest.kt
@@ -0,0 +1,125 @@
+package me.demo.flink.watermark
+
+import me.demo.flink.source.CustomSource
+import me.demo.flink.source.Event
+import org.apache.flink.api.common.eventtime.WatermarkStrategy
+import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.api.java.tuple.Tuple2
+import org.apache.flink.streaming.api.datastream.DataStreamSource
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.time.Time
+import java.sql.Timestamp
+
+class WindowTest {
+
+
+}
+
+fun main() {
+    // 创建执行环境
+    val env = StreamExecutionEnvironment.getExecutionEnvironment()
+    env.parallelism = 1
+    // 水位线100ms生成
+    env.config.autoWatermarkInterval = 100
+
+    // 从输入对象读取
+    val elementStream: DataStreamSource<Event> = env.fromElements(
+        Event("alice", "/cart", 1),
+        Event("alice", "/prod?id=100", 2),
+        Event("alice", "/prod?id=100", 3),
+        Event("alice", "/prod?id=2", 4),
+        Event("alice", "/prod?id=100", 3),
+        Event("alice", "/prod?id=2", 4),
+        Event("bob", "/prod?id=100", 1),
+        Event("bob", "/home", 4)
+    )
+    // 单调递增的有序流的水位线策略
+    elementStream
+        .assignTimestampsAndWatermarks(
+            WatermarkStrategy.forMonotonousTimestamps<Event>()
+                .withTimestampAssigner { element, _ -> element.timestamp }
+        )
+
+    /** TimeWindows */
+    // 滚动时间窗口
+    elementStream
+        .keyBy { it.user }
+        .window(TumblingEventTimeWindows.of(Time.hours(1), Time.minutes(15)))
+
+    // 滑动时间窗口
+    elementStream
+        .keyBy { it.user }
+        .window(SlidingEventTimeWindows.of(Time.hours(1), Time.minutes(15)))
+
+    // 会话时间窗口
+    elementStream
+        .keyBy { it.user }
+        .window(EventTimeSessionWindows.withGap(Time.seconds(2)))
+
+    // 会话时间窗口
+    elementStream
+        .keyBy { it.user }
+        .window(EventTimeSessionWindows.withGap(Time.seconds(2)))
+
+    /** CountWindows，基于GlobalWindows */
+    // 滚动时间窗口
+    elementStream
+        .keyBy { it.user }
+        .countWindow(3)
+
+    // 计窗时间窗口内用户的访问次数：reduce
+    val output: SingleOutputStreamOperator<Tuple2<String, Int>> =
+        env.addSource(CustomSource())
+            .assignTimestampsAndWatermarks(
+                WatermarkStrategy.forMonotonousTimestamps<Event>()
+                    .withTimestampAssigner { element, _ -> element.timestamp }
+            )
+            .map { Tuple2.of(it.user, 1) }
+            .keyBy { it.f0 }
+            .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+            .reduce { v1, v2 -> Tuple2.of(v1.f0, v1.f1 + v2.f1) }
+    //output.print("PV：TumblingEventTimeWindows")
+
+    // 计窗时间窗口内用户的平均访问时长：reduce
+    val avgOutput: SingleOutputStreamOperator<String> =
+        env.addSource(CustomSource())
+            .assignTimestampsAndWatermarks(
+                WatermarkStrategy.forMonotonousTimestamps<Event>()
+                    .withTimestampAssigner { element, _ -> element.timestamp }
+            )
+            .keyBy { it.user }
+            .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+            .aggregate(MyAggFun())
+    avgOutput.print("APT:TumblingEventTimeWindows")
+    env.execute()
+}
+
+/***
+ * IN：Event
+ * ACC：Tuple2<访问时间,次数>
+ * OUT：平均访问时间
+ */
+class MyAggFun : AggregateFunction<Event, Tuple2<Long, Int>, String> {
+    override fun createAccumulator(): Tuple2<Long, Int> {
+        return Tuple2.of(0, 0)
+    }
+
+    override fun add(value: Event, accumulator: Tuple2<Long, Int>): Tuple2<Long, Int> {
+        return Tuple2.of(accumulator.f0 + value.timestamp, accumulator.f1 + 1)
+    }
+
+    override fun getResult(accumulator: Tuple2<Long, Int>): String {
+        return Timestamp(accumulator.f0 / accumulator.f1).toString()
+    }
+
+    override fun merge(a: Tuple2<Long, Int>, b: Tuple2<Long, Int>): Tuple2<Long, Int> {
+        return Tuple2.of(a.f0 + b.f0, a.f1 + b.f1)
+    }
+
+}
+
+
