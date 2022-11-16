105c4e7969978b3ad3c29a156293ed853f0c9620
wanggang <wanggy01@servyou.com.cn>
2022-11-16 22:16:33 +0800

    feat-各类state测试

--- src/main/java/me/demo/flink/state/StateTest.kt
+++ src/main/java/me/demo/flink/state/StateTest.kt
@@ -3,7 +3,16 @@ package me.demo.flink.state
 import me.demo.flink.source.ClickSource
 import me.demo.flink.source.Event
 import org.apache.flink.api.common.eventtime.WatermarkStrategy
+import org.apache.flink.api.common.functions.AggregateFunction
 import org.apache.flink.api.common.functions.RichFlatMapFunction
+import org.apache.flink.api.common.state.AggregatingState
+import org.apache.flink.api.common.state.AggregatingStateDescriptor
+import org.apache.flink.api.common.state.ListState
+import org.apache.flink.api.common.state.ListStateDescriptor
+import org.apache.flink.api.common.state.MapState
+import org.apache.flink.api.common.state.MapStateDescriptor
+import org.apache.flink.api.common.state.ReducingState
+import org.apache.flink.api.common.state.ReducingStateDescriptor
 import org.apache.flink.api.common.state.ValueState
 import org.apache.flink.api.common.state.ValueStateDescriptor
 import org.apache.flink.configuration.Configuration
@@ -37,23 +46,68 @@ fun main() {
 
 class MyFlatMap : RichFlatMapFunction<Event, String>() {
 
-    var state: ValueState<Event>? = null
+    var valueState: ValueState<Event>? = null
+    var listState: ListState<Event>? = null
+    var mapState: MapState<String, Long>? = null
+    var reducingState: ReducingState<Event>? = null
+    var aggState: AggregatingState<Event, String>? = null
 
-    override fun open(parameters: Configuration?) {
+    override fun open(parameters: Configuration) {
         super.open(parameters)
-        val stateDesc = ValueStateDescriptor<Event>("state", Event::class.java)
-        state = runtimeContext.getState(stateDesc)
+        //state初始化
+        valueState = runtimeContext.getState(ValueStateDescriptor("value-state", Event::class.java))
+        listState = runtimeContext.getListState(ListStateDescriptor("list-state", Event::class.java))
+        mapState = runtimeContext.getMapState(MapStateDescriptor("map-state", String::class.java, Long::class.java))
+        reducingState = runtimeContext.getReducingState(ReducingStateDescriptor("reducing-state",
+            { value1, value2 -> Event(value1.user, value1.url, value2.timestamp) },
+            Event::class.java)
+        )
+        aggState = runtimeContext.getAggregatingState(
+            AggregatingStateDescriptor("agg-state",
+                object : AggregateFunction<Event, Long, String> {
+                    override fun createAccumulator(): Long {
+                        return 0L
+                    }
+
+                    override fun add(value: Event, accumulator: Long): Long {
+                        return accumulator.inc()
+                    }
+
+                    override fun getResult(accumulator: Long): String {
+                        return "count:$accumulator"
+                    }
+
+                    override fun merge(a: Long, b: Long): Long {
+                        return a + b
+                    }
+                }, Long::class.java))
     }
 
-    override fun flatMap(value: Event?, out: Collector<String>?) {
-        //第一次为null
-        println(state!!.value())
+
+    override fun flatMap(value: Event, out: Collector<String>) {
         //后续每次打印当前key的最新值，key之间状态隔离
-        state!!.update(value)
-        println(state!!.value())
+        valueState!!.update(value)
+        println("${value.user}，valueState :${valueState!!.value()}")
+
+        // listState
+        listState!!.add(value)
+        println("${value.user}，listState size :${listState!!.get().toList().size}")
+
+        // mapState
+        if (mapState!!.get(value.user) != null) {
+            mapState!!.put(value.user, mapState!!.get(value.user) + 1)
+            println("${value.user}，get mapState:${mapState!!.get(value.user)}")
+        }
+
+        // reducing state
+        reducingState!!.add(value)
+        println("${value.user}，reducingState  :${reducingState!!.get().timestamp}")
+
+        // agg state
+        aggState!!.add(value)
+        println("${value.user}，aggState  :${aggState!!.get()}")
     }
 
 }
-
 
 
